646eef908ea58820c39b77e1a3afc07683acba7d
Samuele Ciprietti <samuele.ciprietti@gmail.com>
2013-08-21 21:20:38 +0200

    Navigation bar button color (1/2)

--- src/com/android/settings/samuele/NavigationBarSettings.java
+++ src/com/android/settings/samuele/NavigationBarSettings.java
@@ -10,11 +10,15 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference; 
+
 public class NavigationBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+    private static final String NAVIGATION_BUTTON_COLOR = "navigation_button_color"; 
 
-    private ListPreference mNavigationBarHeight; 
+    private ListPreference mNavigationBarHeight;
+    private ColorPickerPreference mNavigationBarButtonColor; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,20 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
                 Settings.System.NAVIGATION_BAR_HEIGHT, 48);
         mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
         mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
+
+	mNavigationBarButtonColor = (ColorPickerPreference) findPreference(NAVIGATION_BUTTON_COLOR);
+        mNavigationBarButtonColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BUTTON_COLOR, -2);
+        if (intColor == -2) {
+            intColor = getResources().getColor(
+                    com.android.internal.R.color.white);
+            mNavigationBarButtonColor.setSummary(getResources().getString(R.string.color_default));
+        } else {
+            String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mNavigationBarButtonColor.setSummary(hexColor);
+        }
+        mNavigationBarButtonColor.setNewPreviewColor(intColor);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -38,7 +56,16 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
             mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
-        }
-        return true; 
+	    return true;
+        } else if (preference == mNavigationBarButtonColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BUTTON_COLOR, intHex);
+            return true;
+	} 
+        return false; 
     }
 }
