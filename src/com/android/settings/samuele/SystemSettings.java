d516e0db966ad08a4a93fbeb36bd04806131b328
Samuele Ciprietti <samuele.ciprietti@gmail.com>
2013-08-25 21:11:01 +0200

    Status and navigation bar transparency (2/2)

--- src/com/android/settings/samuele/SystemSettings.java
+++ src/com/android/settings/samuele/SystemSettings.java
@@ -17,8 +17,13 @@
 package com.android.settings.samuele;
 
 import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver; 
@@ -33,11 +38,20 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.util.Log;
+import android.util.TypedValue; 
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.VolumePanel;
+import android.widget.CheckBox;
+import android.widget.CompoundButton; 
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.widgets.AlphaSeekBar; 
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -53,6 +67,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
+    private static final String KEY_QUICK_SETTINGS = "quick_settings";
 
     private CheckBoxPreference mVolumeWake; 
     private ListPreference mVolumeOverlay; 
@@ -60,6 +75,7 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mKillAppLongpressBack;
     private PreferenceScreen mNotificationPulse;
     private PreferenceScreen mBatteryPulse;
+    private Preference mQuickSettings;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
     private final ArrayList<CheckBoxPreference> mResetCbPrefs
@@ -81,7 +97,9 @@ public class SystemSettings extends SettingsPreferenceFragment implements
                 mVolumeWake.setChecked(Settings.System.getInt(resolver,
                         Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
             }
-        }  
+        }
+
+	mQuickSettings = findPreference(KEY_QUICK_SETTINGS);
 
         mVolumeOverlay = (ListPreference) findPreference(KEY_VOLUME_OVERLAY);
         mVolumeOverlay.setOnPreferenceChangeListener(this);
@@ -117,7 +135,17 @@ public class SystemSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), Settings.System.VOLBTN_MUSIC_CONTROLS,
                     mVolBtnMusicCtrl.isChecked() ? 1 : 0); 
 	} else if (preference == mKillAppLongpressBack) {
-	    writeKillAppLongpressBackOptions(); 
+	    writeKillAppLongpressBackOptions();
+	} else if (preference.getKey().equals("transparency_dialog")) {
+            // getFragmentManager().beginTransaction().add(new
+            // TransparencyDialog(), null).commit();
+            openTransparencyDialog();
+	} else if (preference == mQuickSettings) {
+	    Intent quickSettings = new Intent();
+	    quickSettings.setClassName("com.TwinBlade.QSCP",
+		"com.TwinBlade.QSCP.Main");
+	    quickSettings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	    getActivity().getApplicationContext().startActivity(quickSettings); 
 	} else {
 	    // If we didn't handle it, let preferences handle it.
 	    return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -181,6 +209,191 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private void updateKillAppLongpressBackOptions() {
         mKillAppLongpressBack.setChecked(Settings.Secure.getInt(
             getActivity().getContentResolver(), Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) != 0);
-    } 
-}
+    }
 
+    private void openTransparencyDialog() {
+        getFragmentManager().beginTransaction().add(new AdvancedTransparencyDialog(), null)
+                .commit();
+    } 
+
+    public static class AdvancedTransparencyDialog extends DialogFragment {
+
+        private static final int KEYGUARD_ALPHA = 112;
+
+        private static final int STATUSBAR_ALPHA = 0;
+        private static final int STATUSBAR_KG_ALPHA = 1;
+        private static final int NAVBAR_ALPHA = 2;
+        private static final int NAVBAR_KG_ALPHA = 3;
+
+        boolean linkTransparencies = true;
+        CheckBox mLinkCheckBox, mMatchStatusbarKeyguard, mMatchNavbarKeyguard;
+        ViewGroup mNavigationBarGroup;
+
+        TextView mSbLabel;
+
+        AlphaSeekBar mSeekBars[] = new AlphaSeekBar[4];
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setShowsDialog(true);
+            setRetainInstance(true);
+            linkTransparencies = getSavedLinkedState();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            View layout = View.inflate(getActivity(), R.layout.dialog_transparency, null);
+            mLinkCheckBox = (CheckBox) layout.findViewById(R.id.transparency_linked);
+            mLinkCheckBox.setChecked(linkTransparencies);
+
+            mNavigationBarGroup = (ViewGroup) layout.findViewById(R.id.navbar_layout);
+            mSbLabel = (TextView) layout.findViewById(R.id.statusbar_label);
+            mSeekBars[STATUSBAR_ALPHA] = (AlphaSeekBar) layout.findViewById(R.id.statusbar_alpha);
+            mSeekBars[STATUSBAR_KG_ALPHA] = (AlphaSeekBar) layout
+                    .findViewById(R.id.statusbar_keyguard_alpha);
+            mSeekBars[NAVBAR_ALPHA] = (AlphaSeekBar) layout.findViewById(R.id.navbar_alpha);
+            mSeekBars[NAVBAR_KG_ALPHA] = (AlphaSeekBar) layout
+                    .findViewById(R.id.navbar_keyguard_alpha);
+
+            mMatchStatusbarKeyguard = (CheckBox) layout.findViewById(R.id.statusbar_match_keyguard);
+           mMatchNavbarKeyguard = (CheckBox) layout.findViewById(R.id.navbar_match_keyguard);
+
+            try {
+                // restore any saved settings
+                int alphas[] = new int[2];
+                final String sbConfig = Settings.System.getString(getActivity()
+                        .getContentResolver(),
+                        Settings.System.STATUS_BAR_ALPHA_CONFIG);
+                if (sbConfig != null) {
+                    String split[] = sbConfig.split(";");
+                    alphas[0] = Integer.parseInt(split[0]);
+                    alphas[1] = Integer.parseInt(split[1]);
+
+                    mSeekBars[STATUSBAR_ALPHA].setCurrentAlpha(alphas[0]);
+                    mSeekBars[STATUSBAR_KG_ALPHA].setCurrentAlpha(alphas[1]);
+
+                    mMatchStatusbarKeyguard.setChecked(alphas[1] == KEYGUARD_ALPHA);
+
+                    if (linkTransparencies) {
+                        mSeekBars[NAVBAR_ALPHA].setCurrentAlpha(alphas[0]);
+                        mSeekBars[NAVBAR_KG_ALPHA].setCurrentAlpha(alphas[1]);
+                    } else {
+                        final String navConfig = Settings.System.getString(getActivity()
+                                .getContentResolver(),
+                                Settings.System.NAVIGATION_BAR_ALPHA_CONFIG);
+                        if (navConfig != null) {
+                            split = navConfig.split(";");
+                            alphas[0] = Integer.parseInt(split[0]);
+                            alphas[1] = Integer.parseInt(split[1]);
+                            mSeekBars[NAVBAR_ALPHA].setCurrentAlpha(alphas[0]);
+                            mSeekBars[NAVBAR_KG_ALPHA].setCurrentAlpha(alphas[1]);
+
+                            mMatchNavbarKeyguard.setChecked(alphas[1] == KEYGUARD_ALPHA);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                resetSettings();
+            }
+
+            updateToggleState();
+            mMatchStatusbarKeyguard.setOnCheckedChangeListener(mUpdateStatesListener);
+            mMatchNavbarKeyguard.setOnCheckedChangeListener(mUpdateStatesListener);
+            mLinkCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    linkTransparencies = isChecked;
+                    saveSavedLinkedState(isChecked);
+                    updateToggleState();
+                }
+            });
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setView(layout);
+            builder.setTitle(getString(R.string.transparency_dialog_title));
+            builder.setNegativeButton(R.string.cancel, null);
+            builder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (linkTransparencies) {
+                        String config = mSeekBars[STATUSBAR_ALPHA].getCurrentAlpha() + ";" +
+                                mSeekBars[STATUSBAR_KG_ALPHA].getCurrentAlpha();
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.STATUS_BAR_ALPHA_CONFIG, config);
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.NAVIGATION_BAR_ALPHA_CONFIG, config);
+                    } else {
+                        String sbConfig = mSeekBars[STATUSBAR_ALPHA].getCurrentAlpha() + ";" +
+                                mSeekBars[STATUSBAR_KG_ALPHA].getCurrentAlpha();
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.STATUS_BAR_ALPHA_CONFIG, sbConfig);
+
+                        String nbConfig = mSeekBars[NAVBAR_ALPHA].getCurrentAlpha() + ";" +
+                                mSeekBars[NAVBAR_KG_ALPHA].getCurrentAlpha();
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.NAVIGATION_BAR_ALPHA_CONFIG, nbConfig);
+                    }
+                }
+            });
+
+            return builder.create();
+        }
+
+        private void resetSettings() {
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_ALPHA_CONFIG, null);
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_ALPHA_CONFIG, null);
+        }
+
+        private void updateToggleState() {
+            if (linkTransparencies) {
+                mSbLabel.setText(R.string.transparency_dialog_transparency_sb_and_nv);
+                mNavigationBarGroup.setVisibility(View.GONE);
+            } else {
+                mSbLabel.setText(R.string.transparency_dialog_statusbar);
+                mNavigationBarGroup.setVisibility(View.VISIBLE);
+            }
+
+            mSeekBars[STATUSBAR_KG_ALPHA]
+                    .setEnabled(!mMatchStatusbarKeyguard.isChecked());
+            mSeekBars[NAVBAR_KG_ALPHA]
+                    .setEnabled(!mMatchNavbarKeyguard.isChecked());
+
+            // disable keyguard alpha if needed
+            if (!mSeekBars[STATUSBAR_KG_ALPHA].isEnabled()) {
+                mSeekBars[STATUSBAR_KG_ALPHA].setCurrentAlpha(KEYGUARD_ALPHA);
+            }
+            if (!mSeekBars[NAVBAR_KG_ALPHA].isEnabled()) {
+                mSeekBars[NAVBAR_KG_ALPHA].setCurrentAlpha(KEYGUARD_ALPHA);
+            }
+        }
+
+        @Override
+        public void onDestroyView() {
+            if (getDialog() != null && getRetainInstance())
+                getDialog().setDismissMessage(null);
+            super.onDestroyView();
+        }
+
+        private CompoundButton.OnCheckedChangeListener mUpdateStatesListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateToggleState();
+            }
+        };
+
+        private boolean getSavedLinkedState() {
+            return getActivity().getSharedPreferences("transparency", Context.MODE_PRIVATE)
+                    .getBoolean("link", true);
+        }
+
+        private void saveSavedLinkedState(boolean v) {
+            getActivity().getSharedPreferences("transparency", Context.MODE_PRIVATE).edit()
+                    .putBoolean("link", v).commit();
+        }
+    }
+}
